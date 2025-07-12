4d367a6691892b041efb328ed88a698b724ee520
alexramiller <alexramiller@Alexs-MacBook-Pro-2.local>
2025-07-11 23:04:55 -0400

    Add namechange function

---
+++ assets/js/namechange.js
@@ -0,0 +1,17 @@
+function changeName(){
+    if($("#collapseButton").html()=="View More"){
+        $("#collapseButton").html("View Less");
+    }
+    else{
+        $("#collapseButton").html("View More");
+    }
+}
+
+function changeName2(){
+    if($("#collapseButton2").html()=="View More"){
+        $("#collapseButton2").html("View Less");
+    }
+    else{
+        $("#collapseButton2").html("View More");
+    }
+}
